9df0201426a6affb842d52d31845fae9378f0c7d
Xuyuan Pang <xuyuanp@gmail.com>
2014-02-11 14:03:39 +0800

    add install shell script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+if [[ -s ~/.vimrc ]]; then
+    mv ~/.vimrc ~/.vimrc-`date`.bak
+    ln -s vimrc ~/.vimrc
+fi
+
+if [[ -s ~/.vundle.vim ]]; then
+   mv ~/.vundle.vim ~/.vundle.vim-`date`.bak
+   ln -s vundle.vim ~/.vundle.vim
+fi
+
+mkdir -p ~/.vim/bundle
+git clone https://github.com/gmarik/vundl://github.com/gmarik/Vundle.vim.git ~/.vim/bundle
+
+vim +BundleInstall +qall
