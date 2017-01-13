a139641cf049f6fcf2149cd44ff68a594213949c
Xuyuan Pang <xuyuanp@gmail.com>
2017-01-13 18:27:08 +0800

    Updated install.sh

--- install.sh
+++ install.sh
@@ -2,17 +2,17 @@
 
 # Installation directory
 VIM_DIR=~/.vim
-BUNDLE_DIR=$VIM_DIR/bundle/neobundle.vim
+BUNDLE_DIR=${VIM_DIR}/bundle/neobundle.vim
 NOW=`date "+%m%d%H%M%Y"`
 
 info () {
     echo "=> $1"
 }
 
-if [ -e $VIM_DIR ]; then
-    info "$VIM_DIR already exists"
-    info "Backup $VIM_DIR"
-    mv $VIM_DIR $VIM_DIR.bak.$NOW
+if [ -e ${VIM_DIR} ]; then
+    info "${VIM_DIR} already exists"
+    info "Backup ${VIM_DIR}"
+    mv ${VIM_DIR} ${VIM_DIR}.bak.${NOW}
 fi
 
 # check git command
@@ -25,22 +25,25 @@ fi
 
 # make bundle dir and fetch neobundle
 info "Begin fetching vimrc."
-mkdir -p $VIM_DIR
-git clone https://github.com/Xuyuanp/vimrc $VIM_DIR
+mkdir -p ${VIM_DIR}
+git clone https://github.com/Xuyuanp/vimrc ${VIM_DIR}
 info "Done."
 
 if [ -s ~/.vimrc ]; then
     info "Backup old ~/.vimrc file."
-    mv ~/.vimrc ~/.vimrc.bak.$NOW
+    mv ~/.vimrc ~/.vimrc.bak.${NOW}
 fi
-ln -s ~/.vim/vimrc ~/.vimrc
 
-info "Install NeoBundle"
-info "Don't worry to see some warning like: not found in 'runtimepath': \"autoload/vimproc.vim\""
-mkdir -p $BUNDLE_DIR
-git clone https://github.com/Shougo/neobundle.vim $BUNDLE_DIR
-. $BUNDLE_DIR/bin/neoinstall
-info "Done."
+if [ -s ~/config/nvim/init.vim ]; then
+    info "Backup old init.vim file."
+    mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.bak.${NOW}
+fi
+
+ln -s ~/.vim/vimrc ~/.vimrc
+mkdir -p ~/.config/nvim
+ln -s ~/.vimrc ~/.config/nvim/init.vim
+
+vim -u ~/.vimrc -c "call dein#install() | qall"
 
 echo ""
 info "OK! Happy hacking."
