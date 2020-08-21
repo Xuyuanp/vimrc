a40304d6127a0be88784c1382c7dfbd516c6a1d9
Xuyuan Pang <xuyuanp@gmail.com>
2020-08-21 12:39:12 +0800

    Added lua pls

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,9 +1,13 @@
+local vim = vim
+
 local lsp_status = require('lsp-status')
+lsp_status.register_progress()
+
 local diagnostic = require('diagnostic')
 local completion = require('completion')
 local nvim_lsp = require('nvim_lsp')
-local configs = require('nvim_lsp/configs')
-local util = require('nvim_lsp/util')
+-- local configs = require('nvim_lsp/configs')
+-- local util = require('nvim_lsp/util')
 
 local on_attach = function(client)
   lsp_status.on_attach(client)
@@ -11,10 +15,10 @@ local on_attach = function(client)
   completion.on_attach(client)
 
   -- Keybindings for LSPs
-  vim.fn.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", {noremap = true, silent = true})
+  vim.fn.nvim_set_keymap("n", "<C-]>", "<cmd>lua vim.lsp.buf.definition()<CR>", {noremap = true, silent = true})
   vim.fn.nvim_set_keymap("n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", {noremap = true, silent = true})
   vim.fn.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.implementation()<CR>", {noremap = true, silent = true})
-  vim.fn.nvim_set_keymap("n", "<c-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", {noremap = true, silent = true})
+  vim.fn.nvim_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", {noremap = true, silent = true})
   vim.fn.nvim_set_keymap("n", "1gD", "<cmd>lua vim.lsp.buf.type_definition()<CR>", {noremap = true, silent = true})
   vim.fn.nvim_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", {noremap = true, silent = true})
   vim.fn.nvim_set_keymap("n", "g0", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", {noremap = true, silent = true})
@@ -23,6 +27,7 @@ end
 
 nvim_lsp.gopls.setup{
     on_attach = on_attach,
+    capabilities = lsp_status.capabilities,
     settings = {
         gopls = {
             usePlaceholders = true
@@ -30,10 +35,35 @@ nvim_lsp.gopls.setup{
     }
 }
 
+-- LspInstall vim-language-server
 nvim_lsp.vimls.setup{
     on_attach = on_attach,
+    capabilities = lsp_status.capabilities,
 }
 
 nvim_lsp.pyls.setup{
     on_attach = on_attach,
+    capabilities = lsp_status.capabilities,
+}
+
+-- LspInstall sumneko_lua
+nvim_lsp.sumneko_lua.setup{
+    on_attach = on_attach,
+    capabilities = lsp_status.capabilities,
+    settings = {
+        Lua = {
+            color = {mode = {"Grammar", "Semantic"}},
+            diagnostics = {
+                enable = true,
+                globals = {
+                    "vim", "Color", "c", "Group", "g", "s", "describe", "it", "before_each", "after_each"
+                },
+            },
+            workspace = {
+                library = {
+                    [vim.fn.expand("/usr/local/opt/neovim/share/nvim/runtime/lua")] = true,
+                },
+            },
+        },
+    },
 }
