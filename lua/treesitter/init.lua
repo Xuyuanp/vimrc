bf857e5b2d5323907c1b96aa5f24fe39b6e131ce
Xuyuan Pang <xuyuanp@gmail.com>
2020-08-24 15:35:17 +0800

    Disabled lua highlight of treesitter

--- lua/treesitter/init.lua
+++ lua/treesitter/init.lua
@@ -2,6 +2,7 @@ require'nvim-treesitter.configs'.setup {
     ensure_installed = {"go", "python"},      -- one of "all", "language", or a list of languages
     highlight = {
         enable = true,              -- false will disable the whole extension
+        disable = { "lua" }
     },
     incremental_selection = {
         enable = true
