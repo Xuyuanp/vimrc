e2b479b89af8a9a4ea6d452e4f71c8a1cfd9ab42
Xuyuan Pang <xuyuanp@gmail.com>
2020-10-23 17:12:11 +0800

    Moved floating window to util module

--- lua/dotvim/util/init.lua
+++ lua/dotvim/util/init.lua
@@ -1,3 +1,6 @@
+local vim = vim
+local api = vim.api
+
 local M = {}
 
 function M.Augroup(group, fn)
@@ -5,6 +8,59 @@ function M.Augroup(group, fn)
     vim.api.nvim_command("autocmd!")
     fn()
     vim.api.nvim_command("augroup end")
+end
+
+function M.floating_window(bufnr)
+    local winnr_bak = vim.fn.winnr()
+    local altwinnr_bak = vim.fn.winnr("#")
+
+    local width, height = vim.o.columns, vim.o.lines
+
+    local win_width = math.ceil(width * 0.8) - 4
+    local win_height = math.ceil(height * 0.8)
+    local row = math.ceil((height - win_height) / 2 - 1)
+    local col = math.ceil((width - win_width) / 2)
+
+    -- border
+    local border_opts = {
+        style = "minimal",
+        relative = "editor",
+        width = win_width + 2,
+        height = win_height + 2,
+        row = row - 1,
+        col = col - 1
+    }
+
+    local border_bufnr = api.nvim_create_buf(false, true)
+    local border_lines = { '╔' .. string.rep('═', win_width) .. '╗' }
+    local middle_line = '║' .. string.rep(' ', win_width) .. '║'
+    for _ = 1, win_height do
+        table.insert(border_lines, middle_line)
+    end
+    table.insert(border_lines, '╚' .. string.rep('═', win_width) .. '╝')
+    vim.api.nvim_buf_set_lines(border_bufnr, 0, -1, false, border_lines)
+    local border_winnr = api.nvim_open_win(border_bufnr, true, border_opts)
+    api.nvim_win_set_option(border_winnr, "winblend", 0)
+    api.nvim_win_set_option(border_winnr, "winhl", "NormalFloat:")
+
+    -- content
+    local win_opts = {
+        style = "minimal",
+        relative = "editor",
+        width = win_width,
+        height = win_height,
+        row = row,
+        col = col,
+    }
+    local winnr = api.nvim_open_win(bufnr, true, win_opts)
+
+    api.nvim_command(string.format([[autocmd BufWipeout <buffer> execute "silent bwipeout! %d"]], border_bufnr))
+    api.nvim_command(string.format([[autocmd WinClosed  <buffer> execute "%dwincmd w" | execute "%dwincmd w"]], altwinnr_bak, winnr_bak))
+
+    api.nvim_buf_set_keymap(bufnr, "n", "q", ":q<CR>", {nowait = true, noremap = false, silent = false})
+    api.nvim_buf_set_keymap(bufnr, "n", "<ESC><ESC>", ":q<CR>", {nowait = true, noremap = false, silent = false})
+
+    return winnr
 end
 
 return M
--- lua/dotvim/yanil.lua
+++ lua/dotvim/yanil.lua
@@ -1,6 +1,8 @@
 local vim = vim
 local api = vim.api
 
+local util = require("dotvim/util")
+
 local yanil      = require("yanil")
 local git        = require("yanil/git")
 local decorators = require("yanil/decorators")
@@ -9,42 +11,9 @@ local canvas     = require("yanil/canvas")
 
 local M = {}
 
--- TODO: refactor this shit
 local function git_diff(_tree, node)
     local diff = git.diff(node)
     if not diff then return end
-
-    local winnr_bak = vim.fn.winnr()
-    local altwinnr_bak = vim.fn.winnr("#")
-
-    local width, height = vim.o.columns, vim.o.lines
-
-    local win_width = math.ceil(width * 0.8) - 4
-    local win_height = math.ceil(height * 0.8)
-    local row = math.ceil((height - win_height) / 2 - 1)
-    local col = math.ceil((width - win_width) / 2)
-
-    -- border
-    local border_opts = {
-        style = "minimal",
-        relative = "editor",
-        width = win_width + 2,
-        height = win_height + 2,
-        row = row - 1,
-        col = col - 1
-    }
-
-    local border_bufnr = api.nvim_create_buf(false, true)
-    local border_lines = { '╔' .. string.rep('═', win_width) .. '╗' }
-    local middle_line = '║' .. string.rep(' ', win_width) .. '║'
-    for _ = 1, win_height do
-        table.insert(border_lines, middle_line)
-    end
-    table.insert(border_lines, '╚' .. string.rep('═', win_width) .. '╝')
-    vim.api.nvim_buf_set_lines(border_bufnr, 0, -1, false, border_lines)
-    local border_winnr = api.nvim_open_win(border_bufnr, true, border_opts)
-    api.nvim_win_set_option(border_winnr, "winblend", 0)
-    api.nvim_win_set_option(border_winnr, "winhl", "NormalFloat:")
 
     -- content
     local bufnr = api.nvim_create_buf(false, true)
@@ -53,26 +22,14 @@ local function git_diff(_tree, node)
     api.nvim_buf_set_option(bufnr, "swapfile", false)
     api.nvim_buf_set_lines(bufnr, 0, -1, false, diff)
 
-    local win_opts = {
-        style = "minimal",
-        relative = "editor",
-        width = win_width,
-        height = win_height,
-        row = row,
-        col = col,
-    }
-    local winnr = api.nvim_open_win(bufnr, true, win_opts)
+    local winnr = util.floating_window(bufnr)
 
     api.nvim_win_set_option(winnr, "cursorline", true)
     api.nvim_win_set_option(winnr, "winblend", 0)
     api.nvim_win_set_option(winnr, "winhl", "NormalFloat:")
     api.nvim_win_set_option(winnr, "number", true)
-    api.nvim_command(string.format([[autocmd BufWipeout <buffer> execute "silent bwipeout! %d"]], border_bufnr))
-    api.nvim_command(string.format([[autocmd WinClosed  <buffer> execute "%dwincmd w" | execute "%dwincmd w"]], altwinnr_bak, winnr_bak))
 
     api.nvim_command(string.format([[command! -buffer Apply lua require("yanil/git").apply_buf(%d)]], bufnr))
-    api.nvim_buf_set_keymap(bufnr, "n", "q", ":q<CR>", {nowait = true, noremap = false, silent = false})
-    api.nvim_buf_set_keymap(bufnr, "n", "<ESC><ESC>", ":q<CR>", {nowait = true, noremap = false, silent = false})
 end
 
 function M.setup()
