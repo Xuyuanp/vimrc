191cf31a8cc52b4f3f503377c346cf73d2e280fa
Xuyuan Pang <xuyuanp@gmail.com>
2021-07-11 18:23:37 +0800

    Fixed fzf keymap

--- lua/dotvim/plugins/tools.lua
+++ lua/dotvim/plugins/tools.lua
@@ -87,7 +87,7 @@ return {
             local set_keymap = vim.api.nvim_set_keymap
             set_keymap('n', '<leader>ag', ':Ag<CR>', { silent = true, noremap = true })
             set_keymap('n', '<leader>rg', ':Rg<CR>', { silent = true, noremap = true })
-            set_keymap('n', '<leader>ag', ':AF<CR>', { silent = true, noremap = true })
+            set_keymap('n', '<leader>af', ':AF<CR>', { silent = true, noremap = true })
             set_keymap('n', '<A-m>', ':Commands<CR>', { silent = true, noremap = true })
         end
     },
