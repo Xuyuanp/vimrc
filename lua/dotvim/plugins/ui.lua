ad0f915fe52485f880386d2fa38a0161825495a4
Xuyuan Pang <xuyuanp@gmail.com>
2021-07-16 10:13:12 +0800

    Updated indent blankline char

--- lua/dotvim/plugins/ui.lua
+++ lua/dotvim/plugins/ui.lua
@@ -229,6 +229,7 @@ return {
     {
         'lukas-reineke/indent-blankline.nvim',
         config = function()
+            vim.g.indent_blankline_char = '│'
             vim.g.indent_blankline_use_treesitter = true
             vim.g.indent_blankline_show_first_indent_level = true
             vim.g.indent_blankline_show_trailing_blankline_indent = false
