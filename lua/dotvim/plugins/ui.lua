14f782838b612ce7bfd8bc83d2f45bf66c5942cf
Xuyuan Pang <xuyuanp@gmail.com>
2021-07-16 11:14:47 +0800

    Updated indent blankline config

--- lua/dotvim/plugins/ui.lua
+++ lua/dotvim/plugins/ui.lua
@@ -232,7 +232,7 @@ return {
             vim.g.indent_blankline_char = '│'
             vim.g.indent_blankline_use_treesitter = true
             vim.g.indent_blankline_show_first_indent_level = true
-            vim.g.indent_blankline_show_trailing_blankline_indent = false
+            vim.g.indent_blankline_show_trailing_blankline_indent = true
             vim.g.indent_blankline_filetype_exclude = {
                 'help',
                 'man',
