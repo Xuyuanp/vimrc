59e58c3cdf1e98842f286e47f39725ef69028d00
Xuyuan Pang <xuyuanp@gmail.com>
2021-08-02 19:36:35 +0800

    Add vgit

--- lua/dotvim/plugins/tools.lua
+++ lua/dotvim/plugins/tools.lua
@@ -76,39 +76,251 @@ return {
         'roxma/vim-tmux-clipboard',
     },
 
-    'tpope/vim-fugitive',
-
     {
-        'lewis6991/gitsigns.nvim',
-        requires = {
-            'plenary'
-        },
+        'tanvirtin/vgit.nvim',
+        as = 'vgit',
+        requires = { 'plenary' },
         config = function()
-            require('gitsigns').setup({
-                -- signs = {},
-                keymaps = {
-                    noremap = true,
-                    buffer = true,
-
-                    ['n ]c'] = { expr = true, [[&diff ? ']c' : '<cmd>lua require"gitsigns.actions".next_hunk()<CR>']]},
-                    ['n [c'] = { expr = true, [[&diff ? '[c' : '<cmd>lua require"gitsigns.actions".prev_hunk()<CR>']]},
-
-                    ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-                    ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-                    ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-                    ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-                    ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-                    ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-                    ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-                    ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
-
-                    -- Text objects
-                    ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-                    ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
+            require('vgit').setup({
+                disabled = false,
+                debug = false,
+                hunks_enabled = true,
+                blames_enabled = false,
+                diff_strategy = 'index',
+                diff_preference = 'horizontal',
+                predict_hunk_signs = true,
+                action_delay_ms = 300,
+                predict_hunk_throttle_ms = 300,
+                predict_hunk_max_lines = 50000,
+                blame_line_throttle_ms = 150,
+                show_untracked_file_signs = true,
+                signs = {
+                    VGitViewSignAdd = {
+                        name = 'VGitViewSignAdd',
+                        line_hl = 'VGitViewSignAdd',
+                        text_hl = 'VGitViewTextAdd',
+                        text = '+'
+                    },
+                    VGitViewSignRemove = {
+                        name = 'VGitViewSignRemove',
+                        line_hl = 'VGitViewSignRemove',
+                        text_hl = 'VGitViewTextRemove',
+                        text = '-'
+                    },
+                    VGitSignAdd = {
+                        name = 'VGitSignAdd',
+                        text_hl = 'VGitSignAdd',
+                        num_hl = nil,
+                        line_hl = nil,
+                        text = '┃'
+                    },
+                    VGitSignRemove = {
+                        name = 'VGitSignRemove',
+                        text_hl = 'VGitSignRemove',
+                        num_hl = nil,
+                        line_hl = nil,
+                        text = '┃'
+                    },
+                    VGitSignChange = {
+                        name = 'VGitSignChange',
+                        text_hl = 'VGitSignChange',
+                        num_hl = nil,
+                        line_hl = nil,
+                        text = '┃'
+                    },
                 },
-                current_line_blame = false,
-                use_internal_diff = true
+                hls = {
+                    VGitBlame = {
+                        bg = nil,
+                        fg = '#b1b1b1',
+                    },
+                    VGitDiffAddSign = {
+                        bg = '#a0ff70',
+                        fg = nil,
+                    },
+                    VGitDiffRemoveSign = {
+                        bg = '#ff4090',
+                        fg = nil,
+                    },
+                    VGitDiffAddText = {
+                        fg = '#6a8f1f',
+                        bg = '#3d5213',
+                    },
+                    VGitDiffRemoveText = {
+                        fg = '#a3214c',
+                        bg = '#4a0f23',
+                    },
+                    VGitHunkAddSign = {
+                        bg = '#3d5213',
+                        fg = nil,
+                    },
+                    VGitHunkRemoveSign = {
+                        bg = '#4a0f23',
+                        fg = nil,
+                    },
+                    VGitHunkAddText = {
+                        fg = '#6a8f1f',
+                        bg = '#3d5213',
+                    },
+                    VGitHunkRemoveText = {
+                        fg = '#a3214c',
+                        bg = '#4a0f23',
+                    },
+                    VGitHunkSignAdd = {
+                        fg = '#d7ffaf',
+                        bg = '#4a6317',
+                    },
+                    VGitHunkSignRemove = {
+                        fg = '#e95678',
+                        bg = '#63132f',
+                    },
+                    VGitSignAdd = {
+                        fg = '#a0ff70',
+                        bg = '#282828',
+                    },
+                    VGitSignChange = {
+                        fg = '#f0af00',
+                        bg = '#282828',
+                    },
+                    VGitSignRemove = {
+                        fg = '#ff4090',
+                        bg = '#282828',
+                    },
+                    VGitIndicator = {
+                        fg = '#a6e22e',
+                        bg = nil,
+                    },
+                    VGitBorder = {
+                        fg = '#a1b5b1',
+                        bg = nil,
+                    },
+                    VGitBorderFocus = {
+                        fg = '#7AA6DA',
+                        bg = nil,
+                    },
+                },
+                blame_line = {
+                    hl = 'VGitBlame',
+                    format = function(blame, git_config)
+                        local function round(x)
+                            return x >= 0 and math.floor(x + 0.5) or math.ceil(x - 0.5)
+                        end
+                        local config_author = git_config['user.name']
+                        local author = blame.author
+                        if config_author == author then
+                            author = 'You'
+                        end
+                        local time = os.difftime(os.time(), blame.author_time) / (24 * 60 * 60)
+                        local time_format = string.format('%s days ago', round(time))
+                        local time_divisions = { { 24, 'hours' }, { 60, 'minutes' }, { 60, 'seconds' } }
+                        local division_counter = 1
+                        while time < 1 and division_counter ~= #time_divisions do
+                            local division = time_divisions[division_counter]
+                            time = time * division[1]
+                            time_format = string.format('%s %s ago', round(time), division[2])
+                            division_counter = division_counter + 1
+                        end
+                        local commit_message = blame.commit_message
+                        if not blame.committed then
+                            author = 'You'
+                            commit_message = 'Uncommitted changes'
+                            local info = string.format('%s • %s', author, commit_message)
+                            return string.format(' %s', info)
+                        end
+                        local max_commit_message_length = 255
+                        if #commit_message > max_commit_message_length then
+                            commit_message = commit_message:sub(1, max_commit_message_length) .. '...'
+                        end
+                        local info = string.format('%s, %s • %s', author, time_format, commit_message)
+                        return string.format(' %s', info)
+                    end
+                },
+                blame = {
+                    window = {
+                        border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
+                        border_hl = 'VGitBorder',
+                    },
+                },
+                preview = {
+                    priority = 10,
+                    horizontal_window = {
+                        title = 'Preview',
+                        border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
+                        border_hl = 'VGitBorder',
+                        border_focus_hl = 'VGitBorderFocus'
+                    },
+                    current_window = {
+                        title = 'Current',
+                        border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
+                        border_hl = 'VGitBorder',
+                        border_focus_hl = 'VGitBorderFocus'
+                    },
+                    previous_window = {
+                        title = 'Previous',
+                        border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
+                        border_hl = 'VGitBorder',
+                        border_focus_hl = 'VGitBorderFocus'
+                    },
+                    signs = {
+                        add = 'VGitViewSignAdd',
+                        remove = 'VGitViewSignRemove',
+                    },
+                },
+                history = {
+                    indicator = {
+                        hl = 'VGitIndicator'
+                    },
+                    horizontal_window = {
+                        title = 'Preview',
+                        border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
+                        border_hl = 'VGitBorder',
+                        border_focus_hl = 'VGitBorderFocus'
+                    },
+                    current_window = {
+                        title = 'Current',
+                        border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
+                        border_hl = 'VGitBorder',
+                        border_focus_hl = 'VGitBorderFocus'
+                    },
+                    previous_window = {
+                        title = 'Previous',
+                        border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
+                        border_hl = 'VGitBorder',
+                        border_focus_hl = 'VGitBorderFocus'
+                    },
+                    history_window = {
+                        title = 'Git History',
+                        border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
+                        border_hl = 'VGitBorder',
+                        border_focus_hl = 'VGitBorderFocus'
+                    },
+                },
+                hunk = {
+                    priority = 10,
+                    window = {
+                        border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
+                        border_hl = 'VGitBorder',
+                    },
+                    signs = {
+                        add = 'VGitViewSignAdd',
+                        remove = 'VGitViewSignRemove',
+                    },
+                },
+                hunk_sign = {
+                    priority = 10,
+                    signs = {
+                        add = 'VGitSignAdd',
+                        remove = 'VGitSignRemove',
+                        change = 'VGitSignChange',
+                    },
+                },
             })
-        end
+
+            local set_keymap = vim.api.nvim_set_keymap
+            local opts = { noremap = true, silent = true }
+
+            set_keymap('n', '[c', '<cmd>VGit hunk_up<CR>', opts)
+            set_keymap('n', ']c', '<cmd>VGit hunk_down<CR>', opts)
+        end,
     },
 }
