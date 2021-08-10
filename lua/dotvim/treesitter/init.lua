d36fdb11a7e24930026a67e017d1845c03cc15ac
Xuyuan Pang <xuyuanp@gmail.com>
2021-08-10 18:52:08 +0800

    Removed ts custom_captures

--- lua/dotvim/treesitter/init.lua
+++ lua/dotvim/treesitter/init.lua
@@ -8,65 +8,6 @@ ts_configs.setup({
     highlight = {
         enable = true, -- false will disable the whole extension
         disable = { 'lua' },
-        custom_captures = {
-            ['error'] = 'Error',
-            -- Constants
-            ['constant'] = 'Constant',
-            -- ["constant.builtin"] = "TSConstBuiltin",
-            -- ["constant.macro"] = "TSConstMacro",
-            ['string'] = 'String',
-            -- ["string.regex"] = "TSStringRegex",
-            -- ["string.escape"] = "TSStringEscape",
-            ['character'] = 'Character',
-            ['number'] = 'Number',
-            ['boolean'] = 'Boolean',
-            ['float'] = 'Float',
-            -- ["annotation"] = "TSAnnotation",
-
-            -- Functions
-            ['function'] = 'Function',
-            ['function.builtin'] = 'Function',
-            ['function.macro'] = 'Macro',
-            -- ["parameter"] = "TSParameter",
-            -- ["parameter.reference"] = "TSParameterReference",
-            ['method'] = 'Function',
-            -- ["field"] = "TSField",
-            -- ["property"] = "TSProperty",
-            -- ["constructor"] = "TSConstructor",
-
-            -- Keywords
-            ['conditional'] = 'Conditional',
-            ['repeat'] = 'Repeat',
-            ['label'] = 'Label',
-            ['operator'] = 'Operator',
-            ['keyword'] = 'Keyword',
-            ['keyword.function'] = 'Keyword',
-            ['exception'] = 'Exception',
-
-            ['type'] = 'Type',
-            ['type.builtin'] = 'Type',
-            ['structure'] = 'Structure',
-            ['include'] = 'Include',
-
-            -- for rainbow
-            ['punctuation.delimiter'] = 'Delimiter',
-            -- ["punctuation.bracket"] = "",
-
-            -- variable
-            -- ["variable"] = "TSVariable",
-            -- ["variable.builtin"] = "TSVariableBuiltin",
-
-            -- Text
-            -- ["text"] = "TSText",
-            -- ["text.strong"] = "TSStrong",
-            -- ["text.emphasis"] = "TSEmphasis",
-            -- ["text.underline"] = "TSUnderline",
-            -- ["text.title"] = "TSTitle",
-            -- ["text.literal"] = "TSLiteral",
-            -- ["text.uri"] = "TSURI",
-            --
-            -- ["none"] = "TSNone",
-        },
     },
     incremental_selection = {
         enable = true,
