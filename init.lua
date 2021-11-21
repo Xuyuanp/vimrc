22a3d2f35234c3fd9d41ee88afca9f015f8c29cc
Xuyuan Pang <xuyuanp@gmail.com>
2021-11-21 22:53:11 +0800

    Moved init.vim to init.lua

--- init.lua
+++ init.lua
@@ -1,27 +1,25 @@
-lua <<EOF
 _G.pprint = function(obj)
     print(vim.inspect(obj))
 end
-EOF
 
-lua require('dotvim.settings').setup()
-lua require('dotvim.plugins').setup()
-lua require('dotvim.mappings').setup()
+require('dotvim.settings').setup()
+require('dotvim.plugins').setup()
+require('dotvim.mappings').setup()
 
-" force quit
-command! Q execute('qa!')
+-- force quit
+vim.cmd([[command! Q execute('qa!')]])
 
-augroup dotvim_init
-    autocmd!
+require('dotvim.util').Augroup('dotvim_init', function()
+    vim.cmd([[
     autocmd CursorHold * lua require('dotvim.git.lens').show()
     autocmd CursorMoved,CursorMovedI * lua require('dotvim.git.lens').clear()
 
     autocmd DirChanged * lua require('dotvim.git.head').lazy_load()
 
-    " Highlight yanks
     autocmd TextYankPost * silent! lua vim.highlight.on_yank {timeout=500}
 
     autocmd User PackerComplete :PackerCompile<CR>
-augroup end
+    ]])
+end)
 
-highlight! default link GitLens SpecialComment
+vim.cmd([[highlight! default link GitLens SpecialComment]])
