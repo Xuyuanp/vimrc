eb1950db1c54f40e1aa3ac5b771555c26e71a348
Xuyuan Pang <xuyuanp@gmail.com>
2021-11-24 15:25:29 +0800

    Updated telescope config

--- lua/dotvim/telescope.lua
+++ lua/dotvim/telescope.lua
@@ -3,6 +3,19 @@ local M = {}
 function M.setup()
     local ts = require('telescope')
     ts.setup({
+        defaults = {
+            vimgrep_arguments = {
+                'rg',
+                '--color=never',
+                '--no-heading',
+                '--with-filename',
+                '--line-number',
+                '--column',
+                '--smart-case',
+            },
+            prompt_prefix = '  ',
+        },
+
         extensions = {
             ['ui-select'] = {
                 require('telescope.themes').get_dropdown({}),
