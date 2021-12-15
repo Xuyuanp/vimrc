78fa578f1b2f47d04a12a32ea07f6055002004dd
Xuyuan Pang <xuyuanp@gmail.com>
2021-12-15 22:28:47 +0800

    Updated nerdfonts

--- lua/dotvim/telescope.lua
+++ lua/dotvim/telescope.lua
@@ -20,52 +20,10 @@ function M.setup()
     })
 end
 
-local telescope_select = a.async(function(items, opts, callback)
-    local themes = require('telescope.themes')
-    local actions = require('telescope.actions')
-    local state = require('telescope.actions.state')
-    local pickers = require('telescope.pickers')
-    local finders = require('telescope.finders')
-    local conf = require('telescope.config').values
-
-    local picker_opts = themes.get_dropdown({
-        previewer = false,
-    })
-    pickers.new(picker_opts, {
-        prompt_title = opts.prompt,
-        finder = finders.new_table({
-            results = items,
-            entry_maker = function(item)
-                local formatted = opts.format_item(item)
-                return {
-                    display = formatted,
-                    ordinal = item.name,
-                    value = item,
-                }
-            end,
-        }),
-        sorter = conf.generic_sorter(),
-        attach_mappings = function(prompt_bufnr)
-            actions.select_default:replace(function()
-                local selection = state.get_selected_entry()
-                actions._close(prompt_bufnr, false)
-                callback(selection.value)
-            end)
-
-            actions.close:replace(function()
-                actions._close(prompt_bufnr, false)
-                callback(nil)
-            end)
-
-            return true
-        end,
-    }):find()
-end)
-
 M.nerdfonts = a.wrap(function()
     local items = require('dotvim.util.nerdfonts')
 
-    local item = telescope_select(items, {
+    local item = a.ui.select(items, {
         prompt = 'Pick icon',
         format_item = function(item)
             return string.format('%s %s %s', item.icon, item.code, item.name)
